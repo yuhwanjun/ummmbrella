49130de80fadfc5cf898d212e63c42b7499cd7ba
Hwanjun <yuhwanjun0311@gmail.com>
2023-11-10 19:01:11 +0900

    update

--- test.js
+++ test.js
@@ -3,84 +3,78 @@ import { gsap } from "gsap";
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 // 버튼 요소 가져오기
-const resizeButton = document.getElementById("resize-button");
+// const resizeButton = document.getElementById("resize-button");
 
 THREE.ColorManagement.enabled = false
 /**
  * Models
  */
 const gltfLoader = new GLTFLoader()
-let ummmb = null;
+let gltf;
 
-function loadUmmmbrella(){
-    gltfLoader.load(
-        '/static/models/ummmb/ummmb.gltf',
-        (loadedGltf) => {
-            // console.log(gltf.scene.children[1])
-            ummmb = loadedGltf;
-    
-            ummmb.scene.scale.set(2, 2, 2)
-            ummmb.scene.children[0].material = bodyMaterial
-            ummmb.scene.children[1].material = outMaterial
-            ummmb.scene.children[2].material = inMaterial
-    
-            ummmb.scene.rotation.x = Math.PI * 2
-            ummmb.scene.rotation.y = Math.PI * 2
-            ummmb.scene.rotation.Z = Math.PI * 0.2
-            scene.add(ummmb.scene)
-    
-            updateAllMaterials()
+gltfLoader.load(
+    '/static/models/ummmb/ummmb.gltf',
+    (loadedGltf) => {
+        // console.log(gltf.scene.children[1])
+        gltf = loadedGltf;
 
-            let num = 0;
-            // 버튼 클릭 이벤트 처리
-            resizeButton.addEventListener("click", () => {
-                
-                // gsap을 사용하여 크기를 조절
-                gsap.to(ummmb.scene.scale, {
-                  x: 1,
-                  y: 2.6,
-                  z: 1,
-                  duration: 0.4, // 애니메이션 지속 시간 (초)
-                  ease: "elastic.Out", // 애니메이션 이징
-                  onComplete: () => {
-                    // 확대 애니메이션이 완료된 후에 작아지는 애니메이션 시작
-                    gsap.to(ummmb.scene.scale, {
-                      x: 2, // 작아질 크기 (원래 크기)
-                      y: 2,
-                      z: 2,
-                      duration: .1, // 애니메이션 지속 시간 (1초)
-                      ease: "power4.in", // 애니메이션 이징
-                    });
-                  },
-                });
-    
-                const rotationDuration = 1.1; // 회전 애니메이션 지속 시간 (초)
-                const rotationCount = 1; // 회전 횟수
-              
-                // 모델 회전 애니메이션
-                gsap.to(ummmb.scene.rotation, {
-                  y: "+=" + Math.PI * 2, // Y 축 주위로 한 번 회전
-                  duration: rotationDuration,
-                  repeat: rotationCount - 1, // 초기 회전 포함하여 총 rotationCount - 1번 반복
-                  ease: "power2.Out"
-                });
-    
-                outMaterial.color = new THREE.Color(0xffffff);
-                // inMaterial.map = outColorTex
-                outMaterial.map = chColorTex[num]
-                num += 1;
-                if(num >= 10){
-                    num = 1;
-                }
-    
-                updateAllMaterials(); // 재질 업데이트 함수 호출
-            });
-        }
-    )
-}
-loadUmmmbrella()
-// console.log(ummmb)
+        gltf.scene.scale.set(2, 2, 2)
+        gltf.scene.children[0].material = bodyMaterial
+        gltf.scene.children[1].material = outMaterial
+        gltf.scene.children[2].material = inMaterial
 
+        gltf.scene.rotation.x = Math.PI * 0.15
+        gltf.scene.rotation.y = Math.PI * 0.25
+        scene.add(gltf.scene)
+
+        updateAllMaterials()
+        let num = 0;
+        // 버튼 클릭 이벤트 처리
+        // resizeButton.addEventListener("click", () => {
+            
+        //     // gsap을 사용하여 크기를 조절
+        //     gsap.to(gltf.scene.scale, {
+        //       x: 1,
+        //       y: 2.6,
+        //       z: 1,
+        //       duration: 0.4, // 애니메이션 지속 시간 (초)
+        //       ease: "elastic.Out", // 애니메이션 이징
+        //       onComplete: () => {
+        //         // 확대 애니메이션이 완료된 후에 작아지는 애니메이션 시작
+        //         gsap.to(gltf.scene.scale, {
+        //           x: 2, // 작아질 크기 (원래 크기)
+        //           y: 2,
+        //           z: 2,
+        //           duration: .1, // 애니메이션 지속 시간 (1초)
+        //           ease: "power4.in", // 애니메이션 이징
+        //         });
+        //       },
+        //     });
+
+        //     const rotationDuration = 1.1; // 회전 애니메이션 지속 시간 (초)
+        //     const rotationCount = 1; // 회전 횟수
+          
+        //     // 모델 회전 애니메이션
+        //     gsap.to(gltf.scene.rotation, {
+        //       y: "+=" + Math.PI * 2, // Y 축 주위로 한 번 회전
+        //       duration: rotationDuration,
+        //       repeat: rotationCount - 1, // 초기 회전 포함하여 총 rotationCount - 1번 반복
+        //       ease: "power2.Out"
+        //     });
+
+        //     outMaterial.color = new THREE.Color(0xffffff);
+        //     // inMaterial.map = outColorTex
+        //     outMaterial.map = chColorTex[num]
+        //     num += 1;
+        //     if(num >= 10){
+        //         num = 1;
+        //     }
+
+        //     updateAllMaterials(); // 재질 업데이트 함수 호출
+        // });
+
+    }
+)
 const textureLoader = new THREE.TextureLoader()
 
 let chColorTex = [];
@@ -165,8 +159,8 @@ window.addEventListener('resize', () => {
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(20, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(0, 0.5, 5)
+const camera = new THREE.PerspectiveCamera(70, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(2, .8, 2)
 scene.add(camera)
 
 const directionalLight = new THREE.DirectionalLight('#ffffff', 2)
